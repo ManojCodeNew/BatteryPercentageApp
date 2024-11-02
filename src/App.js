30efa886db2f239de89b8add47685321a0e07dbd
manoj <manojpoojary2004@gmail.com>
2024-11-02 18:02:29 +0530

    final app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BatteryLevel from './Components/BatteryLevel';
+
+function App() {
+  return (
+    <div className="App">
+      <BatteryLevel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BatteryLevel.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import './BatteryLevel.css';
+import charging from './charging.png';
+export default function BatteryLevel() {
+    const [currentBatteryLevel, setCurrentBatteryLevel] = useState(0);
+    const [isCharging, setIsCharging] = useState(false);
+
+    const batteryLevelFinder = async () => {
+        const BatteryLevel = await navigator.getBattery();
+        setCurrentBatteryLevel(Math.floor(BatteryLevel.level * 100));
+        console.log(BatteryLevel.charging);
+        setIsCharging(BatteryLevel.charging?true:false);
+        
+    }
+    useEffect(() => {
+        batteryLevelFinder();
+    }, [currentBatteryLevel,isCharging])
+    return (
+        <>
+        
+        <div
+        style={
+            {
+                color:currentBatteryLevel>=50?"yellow":"red",
+                fontSize:"60px",
+                marginTop:"60%"
+            }
+        }>
+            
+            <h1 >{currentBatteryLevel} %</h1>
+            {
+                isCharging?<img src={charging} alt="charging" />:''
+            }
+        </div>
+        </>
+    )
+}
