0f673b062c74b01426e547a7a54d196d822c44cc
manoj <manojpoojary2004@gmail.com>
2025-11-02 22:05:04 +0530

    Adding amt calculation

--- src/Components/BatteryLevel.jsx
+++ src/Components/BatteryLevel.jsx
@@ -4,40 +4,84 @@ import charging from './charging.png';
 export default function BatteryLevel() {
     const [currentBatteryLevel, setCurrentBatteryLevel] = useState(0);
     const [isCharging, setIsCharging] = useState(false);
-useEffect(()=>{
-    setInterval(()=>{
-        batteryLevelFinder();
-    },1000)
-    
-})
+
+    // Pidmi Start Date
+    const pidmiStartDate = "2/12/2024";
+    const currentCalculationDate = "2025-10-29";
+    const todaysDate = new Date();
+    const calculationStartDate = new Date(currentCalculationDate).getTime();
+    const currentDate = new Date().getTime();
+
+    const differenceInMilliseconds = currentDate - calculationStartDate;
+    const daysPassed = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+
+    // Total Amount Calculation
+    let totalAmount = 56700;
+    totalAmount = totalAmount + (daysPassed * 200);
+
+    useEffect(() => {
+        setInterval(() => {
+            batteryLevelFinder();
+        }, 1000)
+
+    })
     const batteryLevelFinder = async () => {
         const BatteryLevel = await navigator.getBattery();
         setCurrentBatteryLevel(Math.floor(BatteryLevel.level * 100));
-        console.log(BatteryLevel.charging);
-        setIsCharging(BatteryLevel.charging?true:false);
-        
+        setIsCharging(BatteryLevel.charging ? true : false);
+
     }
     useEffect(() => {
         batteryLevelFinder();
-    }, [currentBatteryLevel,isCharging])
+    }, [currentBatteryLevel, isCharging]);
+
+
     return (
-        <>
-        
-        <center>
-        <div
-        style={
-            {
-                color:currentBatteryLevel>=50?"yellow":"red",
-                fontSize:"60px"
-            }
-        }>
-            <p>HAI THIS IS Likith</p>
-            <h1 >{currentBatteryLevel} %</h1>
-            {
-                isCharging?<img src={charging} alt="charging" />:''
-            }
+        <div className='container'>
+            <div className="left-circle"></div>
+            <div
+                className='top-box'
+                style={
+                    {
+                        color: currentBatteryLevel >= 50 ? "green" : "red",
+                        fontSize: "60px",
+                    }
+                }>
+                <h1 >{currentBatteryLevel} %</h1>
+                {
+                    isCharging ? <img src={charging} alt="charging" /> : ''
+                }
+            </div>
+
+            {/* Pidmi container */}
+            <div className="pidmi-container">
+                <h2>PIDMI Total Amount</h2>
+                <div className="date-container">
+                    <div className="startDate">{pidmiStartDate}</div>
+                    <span className='to'>-</span>
+                    <div className="todayDate">{todaysDate.toLocaleDateString()}</div>
+                </div>
+                <h1>₹ {totalAmount}</h1>
+            </div>
+
+            {/* Footer */}
+            <div className="footer">
+                <div className="dot-container">
+                    <p className="dot">
+
+                    </p>
+                    <p className="dot">
+
+                    </p>
+                    <p className="dot">
+
+                    </p>
+                </div>
+
+
+                <p >Developed by Manoj ChandraShekar</p>
+            </div>
+
         </div>
-        </center>
-        </>
     )
 }
